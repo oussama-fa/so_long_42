2ba377fb564ac058d9341cc190e55b83638e6d75
Oussama Farah <oufarah@e1r12p2.1337.ma>
2025-02-01 05:03:04 +0100

    parsing and norm

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: oufarah <oufarah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:49:23 by oufarah           #+#    #+#             */
-/*   Updated: 2025/02/01 04:30:36 by oufarah          ###   ########.fr       */
+/*   Updated: 2025/02/01 05:01:44 by oufarah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,44 +48,6 @@ char	*skip_last(char *s)
 	return (ret);
 }
 
-int	check_line(char *s)
-{
-	if (!s)
-		return (FALSE);
-	while (*s)
-	{
-		if (*s == '\n' && *(s + 1) == '\n')
-			return (FALSE);
-		s++;
-	}
-	return (TRUE);
-}
-
-int	check_validity_map(char *s)
-{
-	int	count_p;
-	int	count_e;
-	int	count_c;
-	int	i;
-
-	i = -1;
-	count_p = 0;
-	count_e = 0;
-	count_c = 0;
-	while (s[++i])
-	{
-		if (s[i] == 'P')
-			count_p++;
-		else if (s[i] == 'E')
-			count_e++;
-		else if (s[i] == 'C')
-			count_c++;
-	}
-	if (count_e != 1 || count_p != 1 || count_c < 1)
-		return (FALSE);
-	return (TRUE);
-}
-
 void	free_map(char **map, int rows)
 {
 	int	i;
@@ -94,53 +56,6 @@ void	free_map(char **map, int rows)
 	while (++i < rows)
 		free(map[i]);
 	free(map);
-}
-
-int	check_same_len(char **map, int rows)
-{
-	size_t	len;
-	int		i;
-
-	len = ft_strlen(map[0]);
-	i = -1;
-	while (++i < rows)
-	{
-		if (ft_strlen(map[i]) != len)
-			return (FALSE);
-	}
-	return (TRUE);
-}
-
-int	check_walls(char **map, int rows, int cols)
-{
-	int	j;
-	int	i;
-
-	j = -1;
-	while (++j < cols)
-	{
-		if (map[0][j] != '1' || map[rows - 1][j] != '1')
-			return (FALSE); // Top or bottom wall is missing
-	}
-	i = 0;
-	while (++i < rows - 1)
-	{
-		if (map[i][0] != '1' || map[i][cols - 1] != '1')
-			return (FALSE); // Left or right wall is missing
-	}
-	return (TRUE);
-}
-
-int	is_map_valid(char **map, int rows)
-{
-	int	cols;
-
-	cols = ft_strlen(map[0]);
-	if (check_same_len(map, rows))
-		return (TRUE);
-	if (check_walls(map, rows, cols))
-		return (TRUE);
-	return (FALSE);
 }
 
 void	parsing(char *av)
@@ -174,7 +89,7 @@ void	parsing(char *av)
 		print_err("Invalide Map\n");
 	map = ft_split(join, '\n');
 	if (!map)
-		return ; // error handling
+		return ;
 	free(join);
 	rows = 0;
 	while (map[rows])
@@ -183,16 +98,3 @@ void	parsing(char *av)
 		print_err("Invalide Map\n");
 	free_map(map, rows);
 }
-
-// jisos
-
-// int check_filename(char *str)
-// {
-// 	int size = ft_strlen(str);
-// 	if(size < 4)
-// 		return(FALSE);
-// 	if((str[size-1] == 'r' && str[size-2] == 'e'
-// 		 && str[size-3] == 'b' && str[size-4] == '.'))
-// 		return(TRUE);
-// 	return (FALSE);
-// }
